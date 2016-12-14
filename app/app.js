9c31bd1d7898dd502584597e1e388a671fb51711
Robert Zacher <robert.zacher@one.verizon.com>
2016-12-14 16:23:54 -0500

    now shows voteform and vote elements in index.html

--- app/app.js
+++ app/app.js
@@ -1,6 +1,4 @@
 'use strict';
 
-// Define the `phonecatApp` module
-var personApp = angular.module('personApp', []);
-//var storage = require('node-persist'); 
 
+var personApp = angular.module('personApp', []);
